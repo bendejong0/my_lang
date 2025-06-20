d343e7dc781f51cca762e098b053ca6bcf1809db
bendejong <55010051+bendejong0@users.noreply.github.com>
2025-06-20 18:10:28 -0500

    the scanner should be finished. main.rs is updated.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-
+mod scanner;
 
 fn main() {
     if let Some(arg1) = env::args().nth(1) {
         println!("{arg1}");
-        
+        scanner::scan(&arg1)
     }
     else{
         println!("No arguments given!");
